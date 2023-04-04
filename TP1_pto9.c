6833a734a296e08c0847760d79c6df38dbb60dc2
segu23 <segundoschmidt@gmail.com>
2023-04-04 17:44:31 -0300

    Consigna

--- TP1_pto9.c
+++ TP1_pto9.c
@@ -2,6 +2,27 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+/*
+Escribir una función recursiva que implemente el método que se describe para saber si
+un número es divisible por 7. Se separa la primera cifra de la derecha, se la multiplica
+por 2, y se resta este producto de lo que queda a la izquierda y así sucesivamente,
+hasta que el resultado obtenido sea un número menor a 70. El número original será
+múltiplo de 7 si el resultado da cero o múltiplo de 7.
+Ejemplos:
+32291 -> 1x2=2.
+3229 - 2 =
+3227 -> 7x2=14.
+322 - 14 =
+308 -> 8x2=16
+30 - 16 =
+14 -> Múltiplo de 7
+divisiblePor7 (32291) => verdadero
+110 -> 0 x 2 = 0
+11 – 0 =
+11 -> No múltiplo de 7
+divisiblePor7 (110) => falso 
+*/
+
 bool divisiblePor7(int numero){
     if(numero < 70){
         return numero % 7 == 0 || numero == 0;
