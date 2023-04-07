6f35c4f838caba2f5dc4d7f8b91f94fb128471fe
Valentin190306 <101421737+Valentin190306@users.noreply.github.com>
2023-04-07 17:50:35 -0300

    Add files via upload

--- TP1_pto5.c
+++ TP1_pto5.c
@@ -6,7 +6,7 @@ char *Puntos(char C[]){
     int largo = strlen(C);
     int i;
     char c1[25];
-    char c2 [25];
+    char c2[25];
     strcpy(c2,".");
     if (largo < 4){ //Caso base
         
@@ -28,13 +28,47 @@ char *Puntos(char C[]){
 
     }
 }    
-    
 
+
+
+int main(){
+    int i;
+    char permitidos[12] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '\0'};
+    int validacion = 1;
+    char c[16];
+    memset(c, 16, '\0');
+    printf("\n << Coloque un numero para que el programa incerte los puntos de los miles (solo se tomara hasta un maximo de 15 caracteres):");
+
+    while(validacion == 1){ //si la cadena ingresada es erronea, pide una nueva hasta que sea valida.
+		validacion=0;
+		fgets(c, 16, stdin);
+		c[strcspn(c, "\n")] = '\0';
+		for (i=0;i<(strlen(c));i++){	//Valido que la cadena solo tenga numeros.
+			if (strrchr(permitidos,  c[i]) == NULL){
+				validacion=1;
+				i= strlen(c);
+                memset(c, 16, '\0');
+				printf("\n >! Entrada invalida; valor fuera de rago");
+				printf("\n << Coloque un numero para que el programa incerte los puntos de los miles (solo se tomara hasta un maximo de 15 caracteres): ");
+			}
+		} 
+		      
+	}
+	while (c[0]=='0' &&  strlen(c)>1){		//Elimino el 0 si es el primer valor de la cadena
+		for (i=0;i<=(strlen(c));i++){
+			c[i]=c[i+1];
+		}
+	}
+	printf("%s",Puntos(c));
+}
+
+
+/*
 int main(){
     int i;
     int validacion = 1;
     char c[16];
-    printf("Coloque un numero para que el programa incerte los puntos de los miles (solo se tomara hasta un maximo de 15 caracteres):\n");
+    printf("\n << Coloque un numero para que el programa incerte los puntos de los miles (solo se tomara hasta un maximo de 15 caracteres):");
 
     while(validacion == 1){ //si la cadena ingresada es erronea, pide una nueva hasta que sea valida.
 		validacion=0;
@@ -57,3 +91,4 @@ int main(){
 	}
 	printf("%s",Puntos(c));
 }
+*/
