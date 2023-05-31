c0f332446206d6ac9fcfb01bc486b8f176f82728
Valentin190306 <101421737+Valentin190306@users.noreply.github.com>
2023-05-31 11:05:58 -0300

    Add files via upload

--- TP5/arboles_utilidades.c
+++ TP5/arboles_utilidades.c
@@ -63,11 +63,10 @@ void mostrar_arbol_binario (NodoArbol n, enum Recorrido r) {
     if (n == NULL) return;
 
     bool valido = true;
-    printf("\n Gate %d", __LINE__);
     switch (r) {
         case PRE_ORDEN : {
             Iterador iter = iterador_pre_orden(n);
-            printf("\n>> Recorrido en PRE-ORDEN . . . \n");
+            printf("\n>> Recorrido en PRE-ORDEN <<");
             ln_mostrarLista(iter);
             free(iter);
             break;
@@ -75,7 +74,7 @@ void mostrar_arbol_binario (NodoArbol n, enum Recorrido r) {
 
         case IN_ORDEN : {
             Iterador iter = iterador_in_orden(n);
-            printf("\n>> Recorrido en IN-ORDEN . . . \n");
+            printf("\n>> Recorrido en IN-ORDEN <<");
             ln_mostrarLista(iter);
             free(iter);
             break;
@@ -83,7 +82,7 @@ void mostrar_arbol_binario (NodoArbol n, enum Recorrido r) {
 
         case POST_ORDEN : {
             Iterador iter = iterador_post_orden(n);
-            printf("\n>> Recorrido en POST-ORDEN . . . \n");
+            printf("\n>> Recorrido en POST-ORDEN <<");
             ln_mostrarLista(iter);
             free(iter);
             break;
@@ -91,13 +90,13 @@ void mostrar_arbol_binario (NodoArbol n, enum Recorrido r) {
 
         case BFS : {
             Iterador iter = iterador_bfs(n);
-            printf("\n>> Recorrido en BFS . . . \n");
+            printf("\n>> Recorrido en BFS <<");
             ln_mostrarLista(iter);
             free(iter);
             break;
         }
 
-        default: printf("\n>! Recorrido invalido...");
+        default: printf("\n>! Recorrido invalido . . .");
         break;
     }
 }
@@ -117,9 +116,7 @@ void preorden(NodoArbol n, Lista lista) {
 Iterador iterador_pre_orden (NodoArbol n) {
     if (n == NULL) return NULL;
     Lista pre_o = l_crear();
-    printf("\n Gate %d", __LINE__);
     preorden(n, pre_o);
-    printf("\n Gate %d", __LINE__);
     return iterador(pre_o);
 }
 
