10b045b1c6b41b280e66e99f8845c5b0884a74c3
S4NTI2000 <103229123+S4NTI2000@users.noreply.github.com>
2023-05-31 01:54:46 -0300

    Add files via upload

--- TP5/TP5_pto_8.c
+++ TP5/TP5_pto_8.c
@@ -3,24 +3,59 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "arbol_binario_punteros.c"
-#include "tipo_elemento.c"
-#include "nodo.c"
-void nodos_arbol(NodoArbol N){
+#include "arbol_binario.h"
+#include "Listas_Arreglos.c"
+#include "colas_punteros.c"
+#include "tipo_elemento.h"
+#include "Validacion.h"
+
+void nodos_arbol(NodoArbol N, bool *NoHay){
     TipoElemento x;
+    
         x = n_recuperar(N);
-        if (n_hijoderecho(N) != NULL || n_hijoizquierdo(N) != NULL ){
+        if (n_hijoizquierdo(N) != NULL ){
             {
+                *NoHay = false;
                 printf(" %d", x->clave);
-                nodos_arbol(n_hijoizquierdo(N));
-                nodos_arbol(n_hijoderecho(N));
+                nodos_arbol(n_hijoizquierdo(N), NoHay);
+                if (n_hijoderecho(N) != NULL ){
+                    nodos_arbol(n_hijoderecho(N), NoHay);
+                }
             }
 
+        }else if (n_hijoderecho(N) != NULL ){
+                    nodos_arbol(n_hijoderecho(N), NoHay);
+                }
+        
+    }
+
+void Nodos_Internos(ArbolBinario(A)){
+    bool NoHay = true;
+    printf("Nodos internos del arbol:\n");
+    nodos_arbol((n_hijoizquierdo(a_raiz(A))), &NoHay);
+    if(NoHay){printf("No hay nodos internos\n");}
+}
+
+
+void nivelint(NodoArbol Q, int Cbuscada, int *h, int c){
+    TipoElemento X;
+    if (Q != NULL) {
+        X= n_recuperar(Q);
+        if (X->clave == Cbuscada) {
+            *h = c;
         }
         else{
-            
+            nivelint(n_hijoizquierdo(Q), Cbuscada, h, c+1);
+            nivelint(n_hijoderecho(Q), Cbuscada, h, c);
         }
     }
+}
+
+int nivel_nodo(ArbolBinario A, int N){
+    int nivel = -1;
+    nivelint(a_raiz(A), N, &nivel, 0);
+    return nivel;
+}
 
 
 void alturaarlbol(NodoArbol Q,  ArbolBinario a, int *n, int acum){
@@ -41,51 +76,98 @@ void alturaarlbol(NodoArbol Q,  ArbolBinario a, int *n, int acum){
 
 int alt_arbolaux(ArbolBinario A){
     int n,acum = 0;
-    alturaarlbol(a_raiz(A), A,&n, acum);
-    return n;
-}
-void nivelint(NodoArbol Q, int Cbuscada, int *h, int c){
-    TipoElemento X;
-    if (Q != NULL) {
-        X= n_recuperar(Q);
-        if (X->clave == Cbuscada) {
-            *h = c;
-        }
-        else{
-            nivelint(n_hijoizquierdo(Q), Cbuscada, h, c+1);
-            nivelint(n_hijoderecho(Q), Cbuscada, h, c);
-        }
+    if(a_es_vacio(A)){
+        return -1;
+    }else if(a_cantidad_elementos(A) == 1){
+        return 0;
+    }else{
+        alturaarlbol(a_raiz(A), A,&n, acum);
+        return n;
     }
 }
 
-int nivel_nodo(ArbolBinario A, int N){
-    int nivel = -1;
-    nivelint(a_raiz(A), N, &nivel, 0);
-    return nivel;
+bool Hojas(NodoArbol N, ArbolBinario A, int *Nivel){ 
+    
+    if (n_hijoderecho(N) != NULL){
+        if (Hojas(n_hijoderecho(N), A, Nivel) == false){
+            return false;
+        }
+    }
+    if (n_hijoizquierdo(N) == NULL){
+        if (*Nivel == 0){
+            *Nivel = nivel_nodo(A , (n_recuperar(N))->clave);
+        }else{
+            if (nivel_nodo(A , (n_recuperar(N))->clave) != *Nivel){
+                return false;
+            }
+        }
+
+    }else {
+        if (Hojas(n_hijoizquierdo(N), A, Nivel) == false){
+            return false;
+        }  
+    }
+    return true;
 }
-bool ingresoEntero(int* n){
+
+
+bool NivelHojas(ArbolBinario A){
+    int Nivel = 0;
+    return Hojas(a_raiz(A), A, &Nivel);
+ }
+
+
+bool ingresoEntero(int* n, bool *letra){
     char s[10];
+    *letra = false;
     bool resultado = true;
     *n=0;
-    printf("Ingrese una clave numérica o '.' para nulo: ");
+    printf("Ingrese una clave numerica o '.' para nulo: ");
     scanf("%s", s);
+    fflush(stdin);
     if (s[0]=='.'){
         resultado = false;
     }else{
         for (int i = 0; s[i] != '\0'; i++) {
             if ((s[i]>='0')&&(s[i]<='9')){
-                *n = *n * 10 + (s[i] - 48);}
+                *n = *n * 10 + (s[i] - 48);
+            }else{*letra = true;}
         }
     }
     return resultado;
 }
+
+bool ComprobarRepetidos(ArbolBinario A, NodoArbol N, int n){
+    bool b = false;
+    if(!a_es_vacio(A) ){
+        if(n_recuperar(N)->clave == n){
+            b = true;
+            return b;
+        }else{
+            if(n_hijoderecho(N) != NULL){b = ComprobarRepetidos(A,n_hijoderecho(N), n);}
+            if(b == false){    
+                if(n_hijoizquierdo(N) != NULL){b = ComprobarRepetidos(A,n_hijoizquierdo(N), n);}
+            }
+            return b;
+        }
+    }
+}
+
 void Cargar_SubArbol(ArbolBinario A, NodoArbol N, int sa){
     TipoElemento X;
     NodoArbol N1;
     int n;
-    bool b;
+    bool b, letra;
+    letra = false;
     if(!a_es_lleno(A)){
-        b= ingresoEntero(&n);
+        b= ingresoEntero(&n, &letra);
+        while((ComprobarRepetidos(A ,a_raiz(A), n) && b) || letra ){
+            if (letra){
+                printf("Valor invalido, ingrese un numero.\n");
+            }else{printf("El valor ingresado ya se encuentra en el arbol, ingrese otro.\n");}
+            b= ingresoEntero(&n, &letra);
+        }
+
         if (b){
             X= te_crear(n);
             
@@ -99,10 +181,10 @@ void Cargar_SubArbol(ArbolBinario A, NodoArbol N, int sa){
     }    
 }
 
+//Funcion para la carga del arbol
 void cargar_arbol_binario(ArbolBinario A){
     Cargar_SubArbol(A, NULL, 0);
 }
-
 void pre_orden(NodoArbol N){
     TipoElemento x;
     if (N == NULL) {
@@ -116,28 +198,53 @@ void pre_orden(NodoArbol N){
     }
 }
 int main(){
-    int n, nivel;
+    int n, nivel, altura;
+    char filtro[100];
     NodoArbol N;
     ArbolBinario A1;
+    TipoElemento X;
+    
+    system("cls");
     A1 = a_crear();
     cargar_arbol_binario(A1);
-    printf("--------------------------------------------------------\n");
-    printf("pre order desde la raiz ");
-    pre_orden(a_raiz(A1));
-    printf("\nLa altura del arbol es:\n");
-    printf("\n%d\n",alt_arbolaux(A1) + 1);
-    printf("Ingrese el nodo del que quiere saber el nivel\n");
-    scanf("%d",&n);
-    nivel = nivel_nodo(A1, n);
-    if (nivel == -1){
-        printf("La clave no se encuentra en el arbol\n");
-    }
-    else{
-        printf("La clave %d se encuentra en el nivel %d del arbol\n",n,nivel);
-    }
-    printf("Los nodos internos del arbol son:");
-    //no anda
-    nodos_arbol(a_raiz(A1));
-    system("pause");
+    
+    if(a_es_vacio(A1)){
+        printf("\nEl arbol esta vacio\n");    
+    }else{
+        printf("--------------------------------------------------------\n");
+        printf("pre order desde la raiz ");
+        pre_orden(a_raiz(A1));
+        
+        altura = alt_arbolaux(A1);
+        if(altura == -1){
+            printf("\nEl arbol esta vacio\n");    
+        }else{
+            printf("\nLa altura del arbol es: %d\n", altura + 1);
+        }
+        printf("Ingrese el nodo del que quiere saber el nivel\n");
+        fgets(filtro, 100, stdin);
+        n = EntradaEntera(filtro, 0, 0, 0);
+        nivel = nivel_nodo(A1, n);
+        if (nivel == -1){
+            printf("La clave no se encuentra en el arbol\n");
+        }
+        else{
+            printf("La clave %d se encuentra en el nivel %d del arbol\n",n,nivel);
+        }
+        
+        
+        Nodos_Internos(A1);
+        printf("\n");
 
+        if(a_es_vacio(A1)|| a_cantidad_elementos(A1) == 1){
+            printf("No hay hojas, el arbol solo tiene raiz\n");
+        }else{  
+            if(NivelHojas(A1)){
+                printf("Todas las hojas estan al mismo nivel\n");
+            }else{
+                printf("Las hojas no estan al mismo nivel\n");
+            }
+        }
+    }
+    system("pause");
 }
